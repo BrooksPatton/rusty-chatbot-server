4866fa8716c51f7ce307a8aa0ea81d81aaac13af
Brooks Patton <brooks@brookzerker.com>
2018-08-17 10:19:45 -0600

    created a websocket server that mirrors what it is sent

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate rusty_chatbot_server;
+
+use rusty_chatbot_server::run;
+use rusty_chatbot_server::ServerConfig;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+  let twitch_oauth_token = env::var("TWITCH_OAUTH").unwrap();
+  let ip_address = "127.0.0.1:3002";
+  let nickname = "brookzerker_bot";
+  let channel_name = "#brookzerker";
+  let config = ServerConfig::new(ip_address, twitch_oauth_token, nickname, channel_name);
+
+  run(config);
 }
