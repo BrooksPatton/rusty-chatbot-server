11d4748f1d6c91b4c1c6095480045b1b8fa983da
Brooks Patton <brooks@brookzerker.com>
2018-08-22 07:55:27 -0600

    added message to help debug when missing token

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,6 @@ impl Config {
 pub fn run(config: Config) {
   let (tx, rx) = mpsc::channel();
   websocket::run(tx, config.websocket_ip_address);
-  // chatbot::run(rx, config.twitch_token, config.twitch_nickname, config.twitch_channel_name);
   chatbot::run(config.twitch_token, config.twitch_nickname, config.twitch_channel_name, rx);
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rusty_chatbot_server::Config;
 use std::env;
 
 fn main() {
-  let twitch_token = env::var("TWITCH_OAUTH").unwrap();
+  let twitch_token = env::var("TWITCH_OAUTH").expect("Twitch oauth token missing");
   let twitch_nickname = String::from("brookzerker_bot");
   let twitch_channel_name = String::from("#brookzerker");
   let websocket_ip_address = String::from("127.0.0.1:3002");
