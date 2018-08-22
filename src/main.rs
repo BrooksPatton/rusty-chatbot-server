7c60639bf21228c70375eff74db355b6ec47e56a
Brooks Patton <brooks@brookzerker.com>
2018-08-22 09:07:10 -0600

    changed from websockets on localhost to 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
   let twitch_token = env::var("TWITCH_OAUTH").expect("Twitch oauth token missing");
   let twitch_nickname = String::from("brookzerker_bot");
   let twitch_channel_name = String::from("#brookzerker");
-  let websocket_ip_address = String::from("127.0.0.1:3002");
+  let websocket_ip_address = String::from("0.0.0.0:3002");
   let config = Config::new(twitch_token, twitch_nickname, twitch_channel_name, websocket_ip_address);
 
   rusty_chatbot_server::run(config);
